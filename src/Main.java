9a8166d435424732413ae68a1d874ffbf0ed09ed
Chan Sarun <chansarun0@gmail.com>
2024-07-18 19:39:28 +0700

    exercise 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 calculator calculator = new calculator();
